0bec979ff4b9b98715d77296b26ae93a10be8a0b
Ifeoma <osegboifeoma3@gmail.com>
2026-07-29 20:17:09 +0200

    Add Footer component with copyright notice

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-8 text-center">
+      <p>© {new Date().getFullYear()} Ifeoma Osegbo. All rights reserved.</p>
+    </footer>
+  );
+}
