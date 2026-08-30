f560a5ca6f68a994a0f92c03c166f6047d2f85b8
Ifeoma <osegboifeoma3@gmail.com>
2026-08-30 17:50:10 +0200

    Refactor Home component: implement smooth scrolling for section navigation based on URL parameters

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
 import Hero from "../components/sections/home/Hero";
 import AboutSection from "../components/sections/home/AboutSection";
 import Technologies from "../components/sections/home/Technologies";
@@ -6,6 +9,65 @@ import Certifications from "../components/sections/home/Certifications";
 import CTA from "../components/sections/home/CTA";
 
 export default function Home() {
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const section = searchParams.get("section");
+
+    if (section) {
+      setTimeout(() => {
+        const element = document.getElementById(section);
+
+        if (element) {
+          element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        }
+      }, 100);
+    }
+  }, [searchParams]);
+
+  return (
+    <>
+      <Hero />
+      <AboutSection />
+      <Technologies />
+      <FeaturedProjects />
+      <Certifications />
+      <CTA />
+    </>
+  );
+}import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+import Hero from "../components/sections/home/Hero";
+import AboutSection from "../components/sections/home/AboutSection";
+import Technologies from "../components/sections/home/Technologies";
+import FeaturedProjects from "../components/sections/home/FeaturedProjects";
+import Certifications from "../components/sections/home/Certifications";
+import CTA from "../components/sections/home/CTA";
+
+export default function Home() {
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const section = searchParams.get("section");
+
+    if (section) {
+      setTimeout(() => {
+        const element = document.getElementById(section);
+
+        if (element) {
+          element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        }
+      }, 100);
+    }
+  }, [searchParams]);
+
   return (
     <>
       <Hero />
