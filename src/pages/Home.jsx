f2dbe81cbcf1bcadef92574e972bb1a6bedcc75e
Ifeoma <osegboifeoma3@gmail.com>
2026-08-30 15:02:37 +0200

    Add Certifications section to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "../components/sections/home/Hero";
 import AboutSection from "../components/sections/home/AboutSection";
 import Technologies from "../components/sections/home/Technologies";
 import FeaturedProjects from "../components/sections/home/FeaturedProjects";
+import Certifications from "../components/sections/home/Certifications";
 import CTA from "../components/sections/home/CTA";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <AboutSection />
       <Technologies />
       <FeaturedProjects />
+      <Certifications />
       <CTA />
     </>
   );
